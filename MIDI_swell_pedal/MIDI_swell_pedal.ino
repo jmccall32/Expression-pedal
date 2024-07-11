e589a703e00c8a173ff091230d74af033ab1a109
jmccall32 <jmccall32@users.noreply.github.com>
2024-07-11 09:40:56 -0700

    Separated functions for linear & log taper pots

--- MIDI_swell_pedal/MIDI_swell_pedal.ino
+++ MIDI_swell_pedal/MIDI_swell_pedal.ino
@@ -5,20 +5,23 @@
 const uint8_t channel = 0; // zero-based
 const uint8_t swellCC = 11;
 
+// Analog pin to read swell pedal position from
+const uint8_t swellPin = A2;
+
 // Values for cycle timing
 const uint8_t cycleTime_ms = 20; // minimum time between sending control codes
 unsigned long cycleEndTime_ms = 0;
 
 // Swell pedal position, 0-127
-uint8_t swellValue = 0;
-uint8_t oldSwellValue = 0;
+uint8_t swellValue = 127;
+uint8_t oldSwellValue = 127;
 
 // Serial monitor for debugging
 const uint16_t baudRate = 19200;
 
 void setup()
 {
-  swellValue = getSwellValue();
+  swellValue = getSwellValue_log();
   sendCC(channel, swellCC, swellValue);
   oldSwellValue = swellValue;
   
@@ -30,7 +33,7 @@ void setup()
 
 void loop()
 {
-  swellValue = getSwellValue();
+  swellValue = getSwellValue_log();
   if (swellValue != oldSwellValue) // send new CC only on change
   {
     sendCC(channel, swellCC, swellValue);
@@ -45,25 +48,34 @@ void loop()
   cycleEndTime_ms += cycleTime_ms;
 }
 
-uint8_t getSwellValue()
+uint8_t getSwellValue_log()
 {
   /*
-    This function "de-tapers" a log/audio taper pot using a lookup table and
-    linear interpolation
-    
-    If the pedal you're using has a linear pot, this function could be as simple
-    as scaling the 10-bit analog input down to a 7-bit MIDI value, e.g.:
-      uint8_t swellValue = analogRead(swellPin)>>3;
+    This function "de-tapers" the input from a log/audio taper pot using
+    a lookup table and linear interpolation
   */
-  
-  static const uint8_t swellPin = A2; // Analog pin to read swell pedal position from
 
-  // Assumes a typical audio taper pot with 2-part piecewise linear curve
+  // Assumes a typical audio taper pot with 2-part piecewise linear curve.
+  // Crossover point is 10% output at 50% position.
   static const double rawAnalogCounts[3]  = { 0,  102,  1023};
-  static const double rawSwellValue[3]    = { 0,  64,   127};
+  static const double rawSwellValue[3]    = { 0,  63,   127};
   
   double floatSwellValue = Interpolation::Linear(rawAnalogCounts,rawSwellValue,3,analogRead(swellPin),false);
   uint8_t swellValue = int(round(floatSwellValue));
+
+  return swellValue;
+}
+
+uint8_t getSwellValue_lin()
+{
+  /*
+    Use this function if your pedal has a linear pot.
+    
+    This function simply scales the 10-bit analog input down to a 7-bit MIDI
+    value by shifting right 3 bits.
+  */
+
+  uint8_t swellValue = analogRead(swellPin)>>3;
 
   return swellValue;
 }
